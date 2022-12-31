e8e164de1848d539b5118f6cec639414d74d645e
fee8ack21 <fee8ack21@gmail.com>
2022-12-31 12:31:32 +0800

    [API] 1.swagger token UI

--- PJDesignBackStageAPI/App.PL/Startup.cs
+++ PJDesignBackStageAPI/App.PL/Startup.cs
@@ -38,6 +38,30 @@ namespace App.PL
                     Description = "PJ Design APIs"
                 });
 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
